6305d89f47dec685eaab2ffe37d2b555a220bd72
AdamMusluev <player.300@mail.ru>
2022-10-06 11:40:56 +0300

    ready

--- routes/trainers.route.js
+++ routes/trainers.route.js
@@ -1,6 +1,5 @@
 const { Router } = require('express')
 const { trainersController } = require('../controllers/trainers.controller')
-const authMiddlewares = require('../middlewares/auth.middlewares')
 const addFileMiddleware = require("../middlewares/addFile.middleware");
 const router = Router()
 
